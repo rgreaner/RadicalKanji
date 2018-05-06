b43bb8fe51852656108596500d6eb5c8bc27ef79
BrittanyG <brin.greaner@gmail.com>
2018-05-05 23:34:10 -0500

    Time to test the backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,27 +8,28 @@ module.exports = {
     //     .then(function(dbUser)
     // }
     findById: function(req, res) {
+        console.log("we made it", req);
         db.User
         .findById(req.params.id)
         .then(dbUser => res.json(dbUser));
-    },
-
-    create: function(req, res) {
-        db.User
-        .create(req.body)
-        .then(dbUser => res.json(dbUser));
-    },
-
-    update: function(req, res) {
-        db.User
-        .findOneAndUpdate({_id: req.params.id}, req.body)
-        .then(dbUser => res.json(dbUser));
-    },
-    
-    remove: function(req, res) {
-        db.User
-        .findById({_id: req.params.id})
-        .then(dbUser => dbUser.remove())
-        .then(dbUser => res.json(dbUser));
     }
+
+    // create: function(req, res) {
+    //     db.User
+    //     .create(req.body)
+    //     .then(dbUser => res.json(dbUser));
+    // },
+
+    // update: function(req, res) {
+    //     db.User
+    //     .findOneAndUpdate({_id: req.params.id}, req.body)
+    //     .then(dbUser => res.json(dbUser));
+    // },
+    
+    // remove: function(req, res) {
+    //     db.User
+    //     .findById({_id: req.params.id})
+    //     .then(dbUser => dbUser.remove())
+    //     .then(dbUser => res.json(dbUser));
+    // }
 };
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const apiRoutes = require("./api");
 
 //API Routes
 router.use("/api", apiRoutes);
-
+//console.log("did we make it here?")
 //If no API routes hit, send the React App
 router.use(function(req, res) {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
